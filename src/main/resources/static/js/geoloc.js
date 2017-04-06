1cbf76000f4e5d126cf82669679ca385f15bae11
Leila Svantro <leila.svantro@gmail.com>
2017-04-06 10:59:20 +0200

    geo location default fix

--- src/main/resources/static/js/geoloc.js
+++ src/main/resources/static/js/geoloc.js
@@ -1,15 +1,9 @@
 var currentCoordinates = 0;
 var marker = null;
-
-function handleLocationError(browserHasGeolocation) {
-    var errormsg = browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.';
-    alert(errormsg);
-}
+var map;
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 3
     });
@@ -19,8 +13,9 @@ function initMap() {
             lat: event.latLng.lat(),
             lon: event.latLng.lng()
         };
-        console.log(currentCoordinates);
+
         getWeather(currentCoordinates);
+
         map.panTo(event.latLng);
         map.setZoom(5);
 
@@ -57,15 +52,28 @@ function initMap() {
 
             loadLocation(currentCoordinates.lat, currentCoordinates.lon);
 
-            /* TESTING */
-            /*
-             //currentTimeForecast(currentCoordinates);
-             testing5dayForecast(currentCoordinates);
-             getSpecificForecastData(1, 18);
-             */
             getWeather(currentCoordinates);
 
 
+        }, function () {
+            var defaultCoordinates = {
+                lat: 59.407363,
+                lng: 17.946856
+            };
+
+            map.panTo(defaultCoordinates);
+            map.setZoom(5);
+
+            if(marker == null) {
+                marker = new google.maps.Marker({
+                    position: defaultCoordinates,
+                    map: map
+                });
+            } else {
+                marker.setPosition(defaultCoordinates);
+            }
+
+            loadLocation(defaultCoordinates.lat, defaultCoordinates.lng);
         });
     }
 }
@@ -73,11 +81,5 @@ function initMap() {
 
 
 function getWeather(currentCoordinates) {
-
-
     loadLocation(currentCoordinates.lat, currentCoordinates.lon);
-
-    //testing5dayForecast(currentCoordinates);
-    //getSpecificForecastData(1, 18);
-
 }
